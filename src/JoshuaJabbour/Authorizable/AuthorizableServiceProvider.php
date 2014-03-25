543c2ed856e86995c0e2d6b78e298802b4322553
Joshua Jabbour <code@joshuajabbour.com>
2014-03-25 12:28:58 -0700

    Initial laravel package skeleton.

---
+++ src/JoshuaJabbour/Authorizable/AuthorizableServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace JoshuaJabbour\Authorizable;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthorizableServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('joshuajabbour/authorizable');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+}
